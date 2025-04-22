1d1454dc6087329cc9aebf5773aff9cd54ca7da9
edgardotabios <edgardotabios@gmail.com>
2025-04-22 18:15:11 +1200

    1st commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.set('view engine','ejs');
+app.get('/', function (req, res){
+res.render("home");
+});
+app.listen(3000);
+console.log('Node app is running on port 3000');
